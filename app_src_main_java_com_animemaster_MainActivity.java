3bac917d315c51a541e6c689ddaacae96ffa7232
guptaaryan9278 <guptaaryan9278@gmail.com>
2025-09-10 17:12:16 +0530

    It's my order

---
+++ app_src_main_java_com_animemaster_MainActivity.java
@@ -0,0 +1,5 @@
+package com.animemaster;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }}
